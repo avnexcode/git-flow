aba1e6c731ab70995ba181e07640f23737f233ba
Muhammad Fauzi Nur Aziz <102332191+avnexcode@users.noreply.github.com>
2024-09-18 09:53:33 -0400

    page(home): memperbaiki halaman home (#3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Ini Branch Development</h1>
+      <h1>Saya Membuat Branch Page Home untuk memperbaiki HOME</h1>
     </div>
   );
 }
