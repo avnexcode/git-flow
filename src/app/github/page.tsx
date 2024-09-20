d6799939f5e9bb2f2869a85dec91c035a2d37b7a
avnexcode <axnvee18@gmail.com>
2024-09-20 16:37:20 +0700

    page(github): update page github

--- src/app/github/page.tsx
+++ src/app/github/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function Github() {
   return (
-    <div>Github</div>
+    <div>
+        <h1>GitHub</h1>
+        <p>Create New GitHub Page</p>
+    </div>
   )
 }
